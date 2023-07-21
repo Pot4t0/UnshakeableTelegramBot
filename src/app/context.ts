ee1bdb8e9b6018e114c3df37b6d8fe7ff8282b16
Minh <tranhoang.minh@yahoo.com>
2023-07-21 21:31:35 +0800

    Added all modules for easy access

--- src/app/context.ts
+++ src/app/context.ts
@@ -1,4 +1,22 @@
-import { Context, SessionFlavor } from "grammy";
-import { SessionData } from "../models/SessionData";
+import { Context, SessionFlavor } from 'grammy';
+import { SessionData } from '../models/SessionData';
 
 export type BotContext = Context & SessionFlavor<SessionData>;
+
+type Database = Record<string, Array<Record<string, any>>>;
+
+const db = {
+  events: {
+    id: {
+      event: 'ORC',
+      persons: ['p1', 'p2'],
+    },
+    abbd: {},
+  },
+  persons: {
+    id: {
+      name: '',
+      events: ['id1', 'id2'],
+    },
+  },
+};
