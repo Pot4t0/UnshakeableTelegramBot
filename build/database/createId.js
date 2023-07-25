9058e5d2e8a7fb8d7a09ddcf2ac1d0f5f1fe1e04
Minh <tranhoang.minh@yahoo.com>
2023-07-25 20:50:25 +0800

    Remove unnecessary files

--- build/database/createId.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createId = void 0;
-const nanoid_1 = require("nanoid");
-const createId = () => (0, nanoid_1.nanoid)(16);
-exports.createId = createId;
