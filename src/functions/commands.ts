4ce2e21b8882e0e273ac44cb2deae58951c334ff
Minh <tranhoang.minh@yahoo.com>
2023-07-25 20:42:54 +0800

    - Changed Backend to MongoDB - Added name tagging

--- src/functions/commands.ts
+++ src/functions/commands.ts
@@ -1,73 +1,94 @@
-import { CommandContext, InlineKeyboard } from "grammy";
-import { BotContext } from "../app";
-import { Database } from "../database/db-init";
-import { EventTable } from "../database/Entity/tableEntity";
+import { CommandContext, InlineKeyboard } from 'grammy';
+import { BotContext } from '../app';
+import { Database } from '../database_mongoDB/db-init';
+import { Events, Names } from '../database_mongoDB/Entity/tableEntity';
 
-export const start =  async(ctx:CommandContext<BotContext>) => {
-	await ctx.reply("Welcome to UnShakeable Welfare Telegram Bot")
-}
-export const help =  async(ctx:CommandContext<BotContext>) => {
-	await ctx.reply(`
+/* / start command
+ *  Purpose is to tag the username with the name list inside the "names" collection within UnshakeableDB
+ */
+export const start = async (ctx: CommandContext<BotContext>) => {
+  const name = await Database.getRepository(Names).find();
+  const inlineKeyboard = new InlineKeyboard(
+    name.map((n) => [
+      {
+        text: n.nameText,
+        callback_data: `nameStart-${n.nameText}`,
+      },
+    ])
+  );
+  await ctx.reply(
+    'Welcome to Unshakeable Telegram Bot\nPlease select your name:',
+    {
+      reply_markup: inlineKeyboard,
+    }
+  );
+};
+
+export const help = async (ctx: CommandContext<BotContext>) => {
+  await ctx.reply(`
 	Help List
 	/sendwish -->  Send wishes to upcoming welfare events
 	/admin --> Management of resources/data within this telegram bot (FOR WELFARE TEAM ONLY)
-	`)
-
-}
-export const settings =  async(ctx:CommandContext<BotContext>) => {
-	await ctx.reply("Settings (in development)")
-}
+	`);
+};
+export const settings = async (ctx: CommandContext<BotContext>) => {
+  await ctx.reply('Settings (in development)');
+};
 
 export const sendWish = async (ctx: CommandContext<BotContext>) => {
-	const events = await Database.getRepository(EventTable).find()
-	// query.select("eventTable", "name");
-	const inlineKeyboard = new InlineKeyboard(
-		events.map(event => [{
-			text:event.name,
-			callback_data:`eventName-${event.name}`
-		}]
-		)
-	)
-		
-	await ctx.reply("Choose upcoming Welfare Event ", {
-		reply_markup : inlineKeyboard
-	}
+  const event = await Database.getMongoRepository(Events).find();
+  const inlineKeyboard = new InlineKeyboard(
+    event.map((e) => [
+      {
+        text: e.eventName,
+        callback_data: `eventName-${e.eventName}`,
+      },
+    ])
+  );
 
-	
-)};
-export const admin =  async(ctx:CommandContext<BotContext>) => {
-	const inlineKeyboard = new InlineKeyboard(
-		[
-		[
-		{
-			"text" : "Mangement of Wish Database Table",
-			"callback_data" : "wishManagement"
-		}
-		],[
-		{
-			"text": "Management of Unshakeable Members (in development)",
-			"callback_data" : "nameManagement"
-		}
-		],[
-		{
-			"text": "Management of Welfare Events(in development)",
-			"callback_data" : "eventManagement"
-		}
-		],[
-		{
-			"text": "Management of Welfare Financial Matters(in development)",
-			"callback_data" : "financeManagement"
-		}
-		]
-		]
-	);
+  await ctx.reply('Choose upcoming Welfare Event ', {
+    reply_markup: inlineKeyboard,
+  });
+};
+export const adminWelfare = async (ctx: CommandContext<BotContext>) => {
+  const inlineKeyboard = new InlineKeyboard([
+    [
+      {
+        text: 'Manage Welfare Events',
+        callback_data: 'manageWelfareEvent',
+      },
+    ],
+    [
+      {
+        text: 'Manage Welfare Team',
+        callback_data: 'manageWelfareTeam',
+      },
+    ],
+    [
+      {
+        text: 'See Wishes',
+        callback_data: 'seeWelfareWishes',
+      },
+    ],
+    [
+      {
+        text: 'Send Reminder',
+        callback_data: 'manageReminder',
+      },
+    ],
+  ]);
 
-	await ctx.reply(`
-	Welfare Team Admin Matters
-	
-	Pls take not that this is still experimental as such many functions may not work 100%. If there are any issues pls feedback to developer (Minh). He will OTOT settle it
-	`
-	, {
-		reply_markup : inlineKeyboard
-	}
-)}
+  await ctx.reply(
+    `
+	Welfare Team Admin Matters 
+
+	Welfare Team and Event Management can only be used by Welfare IC / IT Rep (for technical purposes only)
+	All members can view all wishes and send reminders in all welfare events
+	Please respect the privacy of LG members by only using the information when deemed necessary.
+	Please also do not abuse the reminder system and annoy your fellow lifegroup members
+	`,
+    {
+      reply_markup: inlineKeyboard,
+    }
+  );
+};
