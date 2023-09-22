f23fa47cd9c98dd3132428f164eeca08eaddad35
Minh <tranhoang.minh@yahoo.com>
2023-09-22 14:45:55 +0800

    while loop

--- build/api/functions/_adminSFCallbacks.js
+++ build/api/functions/_adminSFCallbacks.js
@@ -78,7 +78,8 @@ const sendNotInReminder_3 = (ctx) => __awaiter(void 0, void 0, void 0, function*
     yield _index_1.gsheet.unshakeableSFSpreadsheet.loadInfo();
     const sheet = yield _index_1.gsheet.unshakeableSFSpreadsheet.sheetsByTitle['Telegram'];
     yield sheet.loadCells();
-    for (let i = 4; i <= totalNames.length + 3; i++) {
+    let i = 4;
+    while (i <= totalNames.length + 3) {
         const time = yield sheet.getCellByA1(`F${i}`);
         const date = new Date(((_a = time.value) === null || _a === void 0 ? void 0 : _a.toString()) || '');
         const offset = (date.getTime() - svcDate.getTime()) / 86400000; // in days
@@ -86,8 +87,9 @@ const sendNotInReminder_3 = (ctx) => __awaiter(void 0, void 0, void 0, function*
             const user = yield _db_init_1.Database.getMongoRepository(_tableEntity_1.Names).find({
                 sfrow: i,
             });
-            const name = yield (0, _db_functions_1.sendMessageUser)(user[0].teleUser, reminder, ctx);
+            yield (0, _db_functions_1.sendMessageUser)(user[0].teleUser, reminder, ctx);
         }
+        i++;
     }
     yield ctx.reply(`Reminder sent!`);
     ctx.session = yield (0, _SessionData_1.initial)();
