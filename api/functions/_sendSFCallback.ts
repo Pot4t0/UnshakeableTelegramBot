616d5e0dbaf83d0bebaacebaafe1d5ba60a5c384
Minh <tranhoang.minh@yahoo.com>
2023-10-01 14:31:16 +0800

    fix sf databse

--- api/functions/_sendSFCallback.ts
+++ api/functions/_sendSFCallback.ts
@@ -101,10 +101,10 @@ export const sendSfEvent_2_yes = async (ctx: Filter<BotContext, 'message'>) => {
 
   if (!collection) {
     const sfevent = new SF_mongo();
-    sfevent.teleUser = await teleUserName;
-    sfevent.attendance = await ['Y', ''];
-    sfevent.sf = await sfmsg;
-    sfevent.timestamp = await new Date();
+    sfevent.teleUser = teleUserName.toString();
+    sfevent.attendance = ['Y', ''];
+    sfevent.sf = sfmsg.toString();
+    sfevent.timestamp = new Date();
     await Database.getMongoRepository(SF_mongo).save(sfevent);
   } else {
     await Database.getMongoRepository(SF_mongo).updateOne(
