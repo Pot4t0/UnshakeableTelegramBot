5cda58d1e022582c3478e0535da0c1620b593fc7
Minh <tranhoang.minh@yahoo.com>
2024-01-08 23:24:22 +0800

    Bug Fix

--- build/api/cmd/settings/_index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.settings = void 0;
-var _settingsCallbacks_1 = require("./_settingsCallbacks");
-Object.defineProperty(exports, "settings", { enumerable: true, get: function () { return _settingsCallbacks_1.settings; } });
