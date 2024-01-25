4996a6484584607f99c1a02159c66d2c148a8b11
Minh <tranhoang.minh@yahoo.com>
2024-01-25 09:53:45 +0800

    Fix

--- build/api/cmd/admin/sf/_adminSFInternal.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.adminSFBotOn = void 0;
-class adminSFBotOn {
-}
-exports.adminSFBotOn = adminSFBotOn;
-adminSFBotOn.manualSFNoFunction = 30;
--- src/gsheets/_index.ts
+++
@@ -1,1 +0,0 @@
-export * as gsheet from './_gsheet_init';
