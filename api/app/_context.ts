c6070398df6c72cc12e658742cc798942335b592
Minh <tranhoang.minh@yahoo.com>
2024-01-31 13:05:14 +0800

    Fix dinner attendance

--- api/app/_context.ts
+++
@@ -1,8 +0,0 @@
-import { Context, SessionFlavor } from 'grammy';
-import { SessionData } from '../models/_SessionData';
-import { Conversation, ConversationFlavor } from '@grammyjs/conversations';
-
-export type BotContext = Context &
-  ConversationFlavor &
-  SessionFlavor<SessionData>;
-export type BotConversation = Conversation<BotContext>;
