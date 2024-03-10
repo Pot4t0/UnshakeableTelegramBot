05d90c231788edb896fb471af5e7aa13679caf68
Minh <tranhoang.minh@yahoo.com>
2024-03-11 02:46:20 +0800

    Fix wish view

--- src/database_mongoDB/functions/_index.ts
+++
@@ -1,8 +0,0 @@
-export * as reminder from './_reminder';
-export * as dbSecurity from './_security';
-export * as dbMessaging from './_messaging';
-export * as wish from './_wish';
-export * as eventDB from './_event';
-export * as team from './_team';
-export * as chat from './_chats';
-export * as gSheetDB from './_sheets';
