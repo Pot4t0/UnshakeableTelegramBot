4924b7e2140cfc1de53e6a1f83e5a0a4b989c186
Minh <tranhoang.minh@yahoo.com>
2024-05-22 14:03:04 +0800

    Fix attendance sending to chat

--- build/api/cmd 3/send/claim/_claimInternal.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logClaimAmountBotOn = void 0;
-/**
- * The botOnType for logging the claim amount.
- */
-exports.logClaimAmountBotOn = 10;
